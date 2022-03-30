8c188a4d69730a6aabbfec246cd49b398eba6f0c
Shivam Jari <sjari@paypal.com>
2022-03-30 19:48:08 +0530

    adding REST controller to test

---
+++ src/main/java/com/shivam/springrest/springrestdemo/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.shivam.springrest.springrestdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/v1")
+public class TestController {
+
+
+
+    @GetMapping("/names")
+    public List<String> getNames()
+    {
+
+        List<String> names = new ArrayList<>();
+        names.add("Shivam");
+        names.add("Mehandi");
+        return names;
+    }
+}
